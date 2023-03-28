563ced5a080a8e0fe808423a625421d039a5275e
ijaeu <lebitances@hanmail.net>
2023-03-28 10:50:42 +0900

    admin 1차

---
+++ WebContent/assets/js/adminLogin.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+let queryString = location.search;
+
+let urlParams = new URLSearchParams(queryString);
+
+let login = urlParams.get('login');
+
+if(login == 'fail'){
+	alert('일치하는 회원 정보가 없습니다.');
+}
+
+if(login == 'noInfo'){
+	alert('로그인 정보가 없습니다.');
+	}
