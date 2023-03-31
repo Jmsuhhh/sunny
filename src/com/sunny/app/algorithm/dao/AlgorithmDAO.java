79d9d88e248c2dcc1ec687f2a1deaa4261abec7d
iosghz <iosghz785@naver.com>
2023-03-31 10:31:03 +0900

    이상한 경로 삭제 및 algorithm 간단한 수정

--- src/com/sunny/app/algorithm/dao/AlgorithmDAO.java
+++ src/com/sunny/app/algorithm/dao/AlgorithmDAO.java
@@ -3,6 +3,7 @@ package com.sunny.app.algorithm.dao;
 import org.apache.ibatis.session.SqlSession;
 
 import com.mybatis.config.MyBatisConfig;
+import com.sunny.app.algorithm.dto.AlgorithmDTO;
 
 public class AlgorithmDAO {
 
@@ -13,7 +14,10 @@ public class AlgorithmDAO {
 		
 		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
 		
+	}
+	
+	public int algorithmResult(AlgorithmDTO algorithmDTO) {
 		
-		
+		return sqlSession.selectOne("algorithm" , algorithmDTO );
 	}
 }
