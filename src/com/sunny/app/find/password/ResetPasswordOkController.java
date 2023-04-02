59b3157abcfba6ffac07e06c445cb707c2840ecf
Jmsuhhh <min567567@naver.com>
2023-04-03 01:16:33 +0900

    회원가입완료 페이지 연결, 비밀번호 관련 컨트롤러 작성 중

---
+++ src/com/sunny/app/find/password/ResetPasswordOkController.java
@@ -0,0 +1,17 @@
+package com.sunny.app.find.password;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sunny.app.Execute;
+
+public class ResetPasswordOkController implements Execute {
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+	}
+}
